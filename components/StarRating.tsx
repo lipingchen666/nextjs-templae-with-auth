f1a5de09dc67f1aa6511901e8ec8dbd23bfa3a8a
lipingchen666 <liping@videoblocks.com>
2023-09-28 14:37:01 -0400

    add some helpful comments on height and width of the target

--- components/StarRating.tsx
+++ components/StarRating.tsx
@@ -28,7 +28,18 @@ const StarRating = ({
 
     const onHover = (e: React.MouseEvent<HTMLDivElement>, i: number) => {
         setIsHovering(true);
+        console.log("e.currentTarget", e.currentTarget.id);
+        console.log("e.target", e.target.id);
+        console.log("client height", e.currentTarget.clientHeight);
+        console.log("client width", e.currentTarget.clientWidth);
+        console.log("offset height", e.currentTarget.offsetHeight);
+        console.log("offset width", e.currentTarget.offsetWidth);
+
         const rect = e.currentTarget.getBoundingClientRect();
+        console.log("rec width", rect.width);
+        console.log("rec height", rect.width);
+
+        // e.client x is the mouse's position in the x-axis from the leftmost point in the viewpoint
         const x = e.clientX - rect.left; //x position within the element.
         if (x <= (rect.width / 2)) {
             setHoverRating(i + 0.5)
@@ -51,10 +62,11 @@ const StarRating = ({
         <div className='flex'>
             {[1, 2, 3, 4, 5].map((_, i) => {
                 const ratingToLookAt = isHovering ? hoverRating : rating;
-                
+
                 return (
-                    <div className={`cursor-pointer ${i < starNum - 1 ? "pr-2" : ""}`} key={i} onMouseMove={e => { onHover(e, i) }} onClick={onClick} onMouseLeave={onMouseLeave}>
+                    <div id="around-div" className={`cursor-pointer ${i < starNum - 1 ? "pr-2" : ""}`} key={i} onMouseMove={e => { onHover(e, i) }} onClick={onClick} onMouseLeave={onMouseLeave}>
                         <Image
+                            id="image-inside"
                             className="w-full h-full"
                             src={getStarSvg(i, ratingToLookAt)}
                             alt={"star"}
